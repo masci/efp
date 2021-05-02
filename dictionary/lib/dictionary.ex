07e0134a515111c7d6806099ce9968bf18994d61
Massimiliano Pippi <mpippi@gmail.com>
2021-05-02 15:48:46 +0200

    pipelines

--- dictionary/lib/dictionary.ex
+++ dictionary/lib/dictionary.ex
@@ -1,10 +1,12 @@
 defmodule Dictionary do
   def word_list do
-    c = File.read!("assets/words.txt")
-    list = String.split(c)
+    "assets/words.txt"
+    |> File.read!()
+    |> String.split()
   end
 
   def random_word do
-    Enum.random(word_list())
+    word_list()
+    |> Enum.random()
   end
 end
