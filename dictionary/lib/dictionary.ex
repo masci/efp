76bd483e937839639600a9fe5639ddab788202be
Massimiliano Pippi <mpippi@gmail.com>
2021-05-02 15:44:53 +0200

    lesson 3-4

--- dictionary/lib/dictionary.ex
+++ dictionary/lib/dictionary.ex
@@ -1,5 +1,10 @@
 defmodule Dictionary do
-  def hello do
-    IO.puts("hello, world!")
+  def word_list do
+    c = File.read!("assets/words.txt")
+    list = String.split(c)
+  end
+
+  def random_word do
+    Enum.random(word_list())
   end
 end
