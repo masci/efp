7f2f2beb617fbad5c0cdde4d13fe261045e3f2b2
Massimiliano Pippi <mpippi@gmail.com>
2022-01-21 22:23:21 +0100

    add tests for a full game session

--- hangman/test/impl_game_test.exs
+++ hangman/test/impl_game_test.exs
@@ -41,4 +41,57 @@ defmodule HangmanTest do
     {game, _tally} = Hangman.Impl.Game.make_move(game, "a")
     assert MapSet.equal?(game.used, MapSet.new(["a", "b"]))
   end
+
+  test "letters in word are recognized" do
+    game = Hangman.Impl.Game.new_game("foo")
+    {_game, tally} = Hangman.Impl.Game.make_move(game, "f")
+    assert tally.game_state == :good_guess
+  end
+
+  test "letters not in word are recognized" do
+    game = Hangman.Impl.Game.new_game("foo")
+    {_game, tally} = Hangman.Impl.Game.make_move(game, "b")
+    assert tally.game_state == :bad_guess
+  end
+
+  test "winning game sequence" do
+    [
+      # guess | state | turns_left | letters | used
+      ["a", :bad_guess, 6, ["_", "_", "_"], ["a"]],
+      ["f", :good_guess, 6, ["f", "_", "_"], ["a", "f"]],
+      ["x", :bad_guess, 5, ["f", "_", "_"], ["a", "f", "x"]],
+      ["a", :already_used, 5, ["f", "_", "_"], ["a", "f", "x"]],
+      ["o", :won, 5, ["f", "o", "o"], ["a", "f", "o", "x"]]
+    ]
+    |> test_game_sequence
+  end
+
+  test "losing game sequence" do
+    [
+      # guess | state | turns_left | letters | used
+      ["a", :bad_guess, 6, ["_", "_", "_"], ["a"]],
+      ["b", :bad_guess, 5, ["_", "_", "_"], ["a", "b"]],
+      ["c", :bad_guess, 4, ["_", "_", "_"], ["a", "b", "c"]],
+      ["d", :bad_guess, 3, ["_", "_", "_"], ["a", "b", "c", "d"]],
+      ["e", :bad_guess, 2, ["_", "_", "_"], ["a", "b", "c", "d", "e"]],
+      ["g", :bad_guess, 1, ["_", "_", "_"], ["a", "b", "c", "d", "e", "g"]],
+      ["h", :lost, 0, ["_", "_", "_"], ["a", "b", "c", "d", "e", "g", "h"]]
+    ]
+    |> test_game_sequence
+  end
+
+  def test_game_sequence(input) do
+    game = Hangman.Impl.Game.new_game("foo")
+    Enum.reduce(input, game, &check_one_move/2)
+  end
+
+  def check_one_move([guess, state, turns, letters, used], g) do
+    {game, tally} = Hangman.Impl.Game.make_move(g, guess)
+    assert tally.game_state == state
+    assert tally.turns_left == turns
+    assert tally.letters == letters
+    assert tally.used == used
+
+    game
+  end
 end
