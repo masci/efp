2cbc62182a24a23448c4de24232f0efcc0f77652
Massimiliano Pippi <mpippi@gmail.com>
2022-01-21 22:24:39 +0100

    finish game logic

--- hangman/lib/impl/game.ex
+++ hangman/lib/impl/game.ex
@@ -46,7 +46,25 @@ defmodule Hangman.Impl.Game do
 
   defp accept_guess(game, guess, _already_used = false) do
     %{game | used: MapSet.put(game.used, guess)}
+    |> score_guess(Enum.member?(game.letters, guess))
   end
+
+  defp score_guess(game, _good_guess = true) do
+    # did we win?
+    new_state = maybe_won(MapSet.subset?(MapSet.new(game.letters), game.used))
+    %{game | game_state: new_state}
+  end
+
+  defp score_guess(game = %{turns_left: 1}, _good_guess = false) do
+    %{game | game_state: :lost, turns_left: 0}
+  end
+
+  defp score_guess(game, _good_guess = false) do
+    %{game | game_state: :bad_guess, turns_left: game.turns_left - 1}
+  end
+
+  defp maybe_won(true), do: :won
+  defp maybe_won(_), do: :good_guess
 
   defp return_with_tally(game) do
     {game, tally(game)}
@@ -56,8 +74,16 @@ defmodule Hangman.Impl.Game do
     %{
       turns_left: game.turns_left,
       game_state: game.game_state,
-      letters: [],
+      letters: reveal_guessed_letters(game),
       used: game.used |> MapSet.to_list() |> Enum.sort()
     }
   end
+
+  defp reveal_guessed_letters(game) do
+    game.letters
+    |> Enum.map(fn letter -> MapSet.member?(game.used, letter) |> maybe_reveal(letter) end)
+  end
+
+  defp maybe_reveal(true, letter), do: letter
+  defp maybe_reveal(_, _), do: "_"
 end
