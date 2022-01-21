d5b7947e0d1665efe4474a92ae21110e1d9c52a6
Massimiliano Pippi <mpippi@gmail.com>
2022-01-21 18:17:53 +0100

    record guess

--- hangman/test/impl_game_test.exs
+++ hangman/test/impl_game_test.exs
@@ -23,4 +23,22 @@ defmodule HangmanTest do
       assert new_game == game
     end
   end
+
+  test "a duplicate letter is reported" do
+    game = Hangman.Impl.Game.new_game("foo")
+    {game, _tally} = Hangman.Impl.Game.make_move(game, "a")
+    assert game.game_state != :already_used
+    {game, _tally} = Hangman.Impl.Game.make_move(game, "b")
+    assert game.game_state != :already_used
+    {game, _tally} = Hangman.Impl.Game.make_move(game, "a")
+    assert game.game_state == :already_used
+  end
+
+  test "letters used are recorded" do
+    game = Hangman.Impl.Game.new_game("foo")
+    {game, _tally} = Hangman.Impl.Game.make_move(game, "a")
+    {game, _tally} = Hangman.Impl.Game.make_move(game, "b")
+    {game, _tally} = Hangman.Impl.Game.make_move(game, "a")
+    assert MapSet.equal?(game.used, MapSet.new(["a", "b"]))
+  end
 end
